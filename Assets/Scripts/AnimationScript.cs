78c37803347536a820ebf83ad1238d28b57b4af1
Aineris1 <128301861+Aineris1@users.noreply.github.com>
2024-01-07 15:20:18 +0200

    update

--- Assets/Scripts/AnimationScript.cs
+++ Assets/Scripts/AnimationScript.cs
@@ -6,27 +6,52 @@ public class AnimationScript : MonoBehaviour
 {
      Animator animator;
     int isWalkingHash;
-
+    int isRunningHash;
     void Start()
     {
        
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
     }
 
     void Update()
     {
+        walk();
+        run();
+    }
+
+    private void run()
+    {
+        bool isRunning = animator.GetBool("isRunning");
+        bool PressedShift = Input.GetKey("left shift");
+        bool forwardPressed = Input.GetKey("w");
+
+        //if player presses shift + w key 
+        if (!isRunning && (forwardPressed && PressedShift))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        //if player doesent press shift + w key 
+        if (isRunning && (!forwardPressed  && !PressedShift))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+
+
+    private void walk()
+    {
         bool isWalking = animator.GetBool("isWalking");
-        bool forwardPressed = Input.GetKey("w"); 
+        bool forwardPressed = Input.GetKey("w");
 
         //if player presses w key 
         if (!isWalking && forwardPressed)
         {
-            
-            animator.SetBool(isWalkingHash, true); 
+            animator.SetBool(isWalkingHash, true);
         }
         //if player doesent press w key 
-        if (isWalking && !forwardPressed) 
+        if (isWalking && !forwardPressed)
         {
             animator.SetBool(isWalkingHash, false);
         }
